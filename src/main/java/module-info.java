38d5ffdee1446d9c3807470d571c3f9f3a14985d
Darmo117 <darmo117@gmail.com>
2023-05-29 13:12:13 +0200

    add comments and remove unnecessary exports in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,10 +7,11 @@ module net.darmo_creations.jenealogio2 {
   requires commons.cli;
   requires com.google.gson;
 
+  // Make AppController open for reflection to JavaFX’s FXML system only
   opens net.darmo_creations.jenealogio2 to javafx.fxml;
+  // Make App accessible to JavaFX, other classes accessible from App’s public interface
+  // are not exported because it’s not necessary
   exports net.darmo_creations.jenealogio2;
+  // Make dialog controllers open for reflection to JavaFX’s FXML system only
   opens net.darmo_creations.jenealogio2.ui to javafx.fxml;
-  exports net.darmo_creations.jenealogio2.utils;
-  exports net.darmo_creations.jenealogio2.config;
-  exports net.darmo_creations.jenealogio2.themes;
 }
