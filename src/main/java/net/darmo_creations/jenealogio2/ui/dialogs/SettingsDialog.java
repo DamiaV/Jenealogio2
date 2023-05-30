32a60e35c574951b3ca6d7d642bd4e329e38463a
Darmo117 <darmo117@gmail.com>
2023-05-30 18:52:36 +0200

    refactor config change detection in SettingsDialog

--- src/main/java/net/darmo_creations/jenealogio2/ui/dialogs/SettingsDialog.java
+++ src/main/java/net/darmo_creations/jenealogio2/ui/dialogs/SettingsDialog.java
@@ -1,7 +1,6 @@
 package net.darmo_creations.jenealogio2.ui.dialogs;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import net.darmo_creations.jenealogio2.App;
 import net.darmo_creations.jenealogio2.config.Config;
@@ -10,7 +9,7 @@ import net.darmo_creations.jenealogio2.themes.Theme;
 
 import java.io.IOException;
 
-public class SettingsDialog extends DialogBase<ButtonType> {
+public class SettingsDialog extends DialogBase<SettingsDialog.ChangeType> {
   @FXML
   @SuppressWarnings("unused")
   private ComboBox<Language> languageCombo;
@@ -18,6 +17,7 @@ public class SettingsDialog extends DialogBase<ButtonType> {
   @SuppressWarnings("unused")
   private ComboBox<Theme> themeCombo;
 
+  private Config initialConfig;
   private Config localConfig;
 
   public SettingsDialog() {
@@ -26,34 +26,47 @@ public class SettingsDialog extends DialogBase<ButtonType> {
     this.languageCombo.getItems().addAll(Config.languages());
     this.themeCombo.getItems().addAll(Theme.themes());
     this.setResultConverter(param -> {
-      if (!param.getButtonData().isCancelButton() && this.configChanged()) {
-        try {
-          App.updateConfig(this.localConfig);
-          this.localConfig.save();
-          Alerts.info("dialog.settings.alert.needs_restart.header", null, null);
-        } catch (IOException e) {
-          App.LOGGER.exception(e);
-          Alerts.error("dialog.settings.alert.save_error.header", null, null);
+      if (!param.getButtonData().isCancelButton()) {
+        ChangeType changeType = this.configChanged();
+        if (changeType.changed()) {
+          try {
+            App.updateConfig(this.localConfig);
+            this.localConfig.save();
+            if (changeType.needsRestart()) {
+              Alerts.info("dialog.settings.alert.needs_restart.header", null, null);
+            }
+          } catch (IOException e) {
+            App.LOGGER.exception(e);
+            Alerts.error("dialog.settings.alert.save_error.header", null, null);
+          }
         }
+        return changeType;
       }
-      return null;
+      return ChangeType.NONE;
     });
   }
 
   public void resetLocalConfig() {
     this.localConfig = App.config().clone();
+    this.initialConfig = this.localConfig.clone();
+
     this.languageCombo.getSelectionModel().select(this.localConfig.language());
     this.themeCombo.getSelectionModel().select(this.localConfig.theme());
+
     this.updateState();
   }
 
   private void updateState() {
-    boolean configChanged = this.configChanged();
+    boolean configChanged = this.configChanged().changed();
     this.getDialogPane().lookupButton(ButtonTypes.OK).setDisable(!configChanged);
   }
 
-  private boolean configChanged() {
-    return !this.localConfig.equals(App.config());
+  private ChangeType configChanged() {
+    if (!this.localConfig.language().equals(this.initialConfig.language())
+        || !this.localConfig.theme().equals(this.initialConfig.theme())) {
+      return ChangeType.NEEDS_RESTART;
+    }
+    return !this.localConfig.equals(this.initialConfig) ? ChangeType.NO_RESTART_NEEDED : ChangeType.NONE;
   }
 
   @FXML
@@ -68,5 +81,28 @@ public class SettingsDialog extends DialogBase<ButtonType> {
   public void onThemeSelect() {
     this.localConfig = this.localConfig.withTheme(this.themeCombo.getSelectionModel().getSelectedItem());
     this.updateState();
+  }
+
+  public enum ChangeType {
+    NONE(false, false),
+    NO_RESTART_NEEDED(true, false),
+    NEEDS_RESTART(true, true),
+    ;
+
+    private final boolean changed;
+    private final boolean needsRestart;
+
+    ChangeType(boolean changed, boolean needsRestart) {
+      this.changed = changed;
+      this.needsRestart = needsRestart;
+    }
+
+    public boolean changed() {
+      return this.changed;
+    }
+
+    public boolean needsRestart() {
+      return this.needsRestart;
+    }
   }
 }
