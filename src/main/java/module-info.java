984026bbd051c8b85e5cb94441afcb01049f46a5
Darmo117 <darmo117@gmail.com>
2023-07-03 00:38:09 +0200

    remove unnecessary exports/opens clauses in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,11 +12,7 @@ module net.darmo_creations.jenealogio2 {
   requires org.threeten.extra;
   requires lib.french.revolutionary.calendar;
 
-  // Make AppController open for reflection to JavaFX’s FXML system only
-  opens net.darmo_creations.jenealogio2 to javafx.fxml;
   // Make App accessible to JavaFX, other classes accessible from App’s public interface
   // are not exported because it’s not necessary
   exports net.darmo_creations.jenealogio2 to javafx.graphics;
-  // Make dialog controllers open for reflection to JavaFX’s FXML system only
-  opens net.darmo_creations.jenealogio2.ui.dialogs to javafx.base, javafx.fxml;
 }
