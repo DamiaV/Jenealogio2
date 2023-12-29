96319a5aa09f7064982512b17019f810ab6459f4
Darmo117 <darmo117@gmail.com>
2023-12-29 20:09:25 +0100

    add some tests for FamilyTree class

---
+++ src/main/test/net/darmo_creations/jenealogio2/model/FamilyTreeTest.java
@@ -0,0 +1,117 @@
+package net.darmo_creations.jenealogio2.model;
+
+import net.darmo_creations.jenealogio2.model.datetime.*;
+import net.darmo_creations.jenealogio2.model.datetime.calendar.Calendar;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SuppressWarnings("DataFlowIssue")
+class FamilyTreeTest {
+  private static LifeEventTypeRegistry typeReg;
+  private FamilyTree tree;
+
+  @BeforeAll
+  static void setUpClass() {
+    typeReg = new LifeEventTypeRegistry();
+  }
+
+  @BeforeEach
+  void setUp() {
+    this.tree = new FamilyTree("tree");
+  }
+
+  @Test
+  void testGetName() {
+    assertEquals("tree", this.tree.name());
+  }
+
+  @Test
+  void testSetName() {
+    this.tree.setName("TREE");
+    assertEquals("TREE", this.tree.name());
+  }
+
+  @Test
+  void testSetNameNullError() {
+    assertThrows(NullPointerException.class, () -> this.tree.setName(null));
+  }
+
+  @Test
+  void testGetPersonsEmpty() {
+    assertTrue(this.tree.persons().isEmpty());
+  }
+
+  @Test
+  void testGetPersonsNotEmpty() {
+    this.tree.addPerson(new Person());
+    this.tree.addPerson(new Person());
+    assertEquals(2, this.tree.persons().size());
+  }
+
+  @Test
+  void testGetLifeEventsEmpty() {
+    assertTrue(this.tree.lifeEvents().isEmpty());
+  }
+
+  @Test
+  void testGetLifeEventsNotEmptyOnePerson() {
+    Person person = new Person();
+    LifeEvent event = new LifeEvent(new DateTimeWithPrecision(Calendar.forName("gregorian").getDate(1970, 1, 1, 1, 0), DateTimePrecision.EXACT), typeReg.getEntry(new RegistryEntryKey("builtin:birth")));
+    this.tree.setLifeEventActors(event, Set.of(person));
+    assertEquals(1, this.tree.lifeEvents().size());
+  }
+
+  @Test
+  void testGetLifeEventsNotEmptyTwoPersons() {
+    Person person1 = new Person();
+    LifeEvent event1 = new LifeEvent(new DateTimeWithPrecision(Calendar.forName("gregorian")
+        .getDate(1970, 1, 1, 1, 0), DateTimePrecision.EXACT),
+        typeReg.getEntry(new RegistryEntryKey("builtin:birth")));
+    this.tree.setLifeEventActors(event1, Set.of(person1));
+    Person person2 = new Person();
+    LifeEvent event2 = new LifeEvent(new DateTimeWithPrecision(Calendar.forName("gregorian")
+        .getDate(1970, 1, 1, 1, 0), DateTimePrecision.EXACT),
+        typeReg.getEntry(new RegistryEntryKey("builtin:birth")));
+    this.tree.setLifeEventActors(event2, Set.of(person2));
+    assertEquals(2, this.tree.lifeEvents().size());
+  }
+
+  @Test
+  void testGetPicturesEmpty() {
+    assertTrue(this.tree.pictures().isEmpty());
+  }
+
+  @Test
+  void testGetPictureEmpty() {
+    assertTrue(this.tree.getPicture("a").isEmpty());
+  }
+
+  @Test
+  @Disabled("Not implemented yet")
+  void testGetPictureNotEmptyOnePerson() {
+    // TODO
+  }
+
+  @Test
+  @Disabled("Not implemented yet")
+  void testGetPictureNotEmptyTwoPersons() {
+    // TODO
+  }
+
+  @Test
+  @Disabled("Not implemented yet")
+  void testGetPictureNotEmptyOneLifeEvent() {
+    // TODO
+  }
+
+  @Test
+  @Disabled("Not implemented yet")
+  void testGetPictureNotEmptyTwoLifeEvents() {
+    // TODO
+  }
+
+  // TODO
+}
