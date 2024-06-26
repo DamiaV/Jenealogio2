9c2b4b87ae01532b6bd72b8546a8a745bb0b2016
Darmo117 <darmo117@gmail.com>
2024-06-26 15:51:46 +0200

    doc

--- src/main/java/net/darmo_creations/jenealogio2/ui/components/ErasableTextField.java
+++ src/main/java/net/darmo_creations/jenealogio2/ui/components/ErasableTextField.java
@@ -8,12 +8,20 @@ import org.jetbrains.annotations.*;
 
 import java.util.*;
 
+/**
+ * A component that holds a {@link TextField} with a {@link Button} to erase the field’s text.
+ */
 public class ErasableTextField extends HBox {
   private final TextField textField = new TextField();
   private final Button eraseButton = new Button();
 
   private final List<EraseListener> eraseListeners = new LinkedList<>();
 
+  /**
+   * Create a new erasable text field.
+   *
+   * @param config The app’s config.
+   */
   public ErasableTextField(@NotNull Config config) {
     super(5);
     HBox.setHgrow(this.textField, Priority.ALWAYS);
@@ -30,10 +38,18 @@ public class ErasableTextField extends HBox {
     this.getChildren().addAll(this.textField, this.eraseButton);
   }
 
+  /**
+   * The wrapped text field.
+   */
   public TextField textField() {
     return this.textField;
   }
 
+  /**
+   * Add a listener that gets notified whenever the erase button is clicked.
+   *
+   * @param listener The listener to add.
+   */
   public void addEraseListener(@NotNull EraseListener listener) {
     this.eraseListeners.add(Objects.requireNonNull(listener));
   }
